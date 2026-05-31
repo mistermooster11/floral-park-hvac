28da8f121a68d4d39a6d9772f747e48c73ed3289
mistermooster11 <mistermooster11@gmail.com>
2026-05-31 09:42:04 +0700

    add sales popup (expires June 7 2026)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/custom/footer/Footer";
 import PageTransition from "@/components/custom/page-transition/PageTransition";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import SalePopup from "@/components/custom/popup/SalePopup"
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -86,6 +87,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         <PageTransition />
+        <SalePopup businessName="Floral Park HVAC" expiryDate="June 7, 2026" trade="HVAC contractors" />
         <Header />
         <main>{children}</main>
         <Footer />
